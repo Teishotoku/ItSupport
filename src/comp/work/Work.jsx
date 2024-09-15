9771a70dc0229893bb09672cd24b5b42c8ad7bf5
Teishotoku <Dl1SqP1L4F0he@protonmail.com>
2024-09-16 01:11:27 +0200

    work block made

--- src/comp/work/Work.jsx
+++ src/comp/work/Work.jsx
@@ -1,12 +1,45 @@
-import './styleWork.scss'
+import "./styleWork.scss";
 
 const Work = () => {
   return (
-    <section>
-
+    <section className="Work">
+      <div className="workCenter">
+        <h1>How we work?</h1>
+        <div className="frames">
+          <div className="workFrameNoBorder">
+            <h1>Requirements analysis and statement of work</h1>
+          </div>
+          <div className="workFrame">
+            <h1>Design (if required)</h1>
+            <p>Some words about this step in our work</p>
+          </div>
+          <div className="workFrame">
+            <h1>Development, show demo version</h1>
+            <p>Some words about this step in our work</p>
+          </div>
+          <div className="workFrame">
+            <h1>Testing, completion</h1>
+            <p>Some words about this step in our work</p>
+          </div>
+          <div className="workFrame">
+            <h1>Product launch</h1>
+            <p>Some words about this step in our work</p>
+          </div>
+          <div className="workFrame workFrameGap">
+            <h1>Continued technical support</h1>
+            <p>
+              Some words about this step in our work. Some words about this step
+              in our work. Some words about this step in our work.
+            </p>
+          </div>
+        </div>
+        <div className="learnRound">
+          <p>Learn</p>
+          <p>More</p>
+        </div>
+      </div>
     </section>
-  )
+  );
+};
 
-}
-
-export default Work
+export default Work;
