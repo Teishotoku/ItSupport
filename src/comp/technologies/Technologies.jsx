8518c3298010d9829b92eacf52c91bcff5dc24f8
Teishotoku <Dl1SqP1L4F0he@protonmail.com>
2024-09-16 03:15:53 +0200

    technologies block made

--- src/comp/technologies/Technologies.jsx
+++ src/comp/technologies/Technologies.jsx
@@ -1,11 +1,89 @@
-import './styleTechnologies.scss'
+import "./styleTechnologies.scss";
+import js_logo from "../../assets/img/java-script.svg";
 
 const Technologies = () => {
   return (
-    <section className='Technologies'>
-
+    <section className="Technologies">
+      <div className="techCenter">
+        <h1>Used technologies</h1>
+        <div className="techP">
+          <div className="techEnd">
+            <p>
+              Fill the gaps in your competencies by setting up a remote IT team.
+              We will find the best experts for you and arrange everything
+              around it. You will get a perfect match both in skills and
+              cultural fit.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="techLine">
+        <div className="techContPrimary">
+          <div className="techContGraph">
+            <div className="techContRoundExtra">
+              <div />
+            </div>
+            <div className="techContLine" />
+          </div>
+          <p>Second technology description</p>
+        </div>
+        <div className="techCont">
+          <div className="techContGraph">
+            <div className="techContRound" />
+            <div className="techContLine" />
+          </div>
+          <p>Second technology description</p>
+        </div>
+        <div className="techCont">
+          <div className="techContGraph">
+            <div className="techContRound" />
+            <div className="techContLine" />
+          </div>
+          <p>Second technology description</p>
+        </div>
+        <div className="techCont">
+          <div className="techContGraph">
+            <div className="techContRound" />
+            <div className="techContLine" />
+          </div>
+          <p>Second technology description</p>
+        </div>
+        <div className="techCont">
+          <div className="techContGraph">
+            <div className="techContRound" />
+            <div className="techContLine" />
+          </div>
+          <p>Second technology description</p>
+        </div>
+        <div className="techCont">
+          <div className="techContGraph">
+            <div className="techContRound" />
+            <div className="techContLine" />
+          </div>
+          <p>Second technology description</p>
+        </div>
+        <div className="techCont">
+          <div className="techContGraph">
+            <div className="techContRound" />
+            <div className="techContLine" />
+          </div>
+          <p>Second technology description</p>
+        </div>
+        <div className="techCont">
+          <div className="techContGraph">
+            <div className="techContRound" />
+            <div className="techContLine" />
+          </div>
+          <p>Second technology description</p>
+        </div>
+      </div>
+      <div className="techIcon">
+        <div>
+          <img src={js_logo} alt="" />
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Technologies
+export default Technologies;
